88ce01955d0180bd9aadf5f2edff6f55aabc0984
evagoel <evagoel2210@gmail.com>
2024-12-03 18:11:37 +0530

    changes made

--- src/app.ts
+++ src/app.ts
@@ -42,7 +42,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer().none());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://entnt-frontend-three.vercel.app"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   }),
 );
